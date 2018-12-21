719bed02e71bf2f72ce3b3e1b789d57bdd392a52
vmkottoo <venu.m.kottooru@intel.com>
2018-12-21 09:35:53 -0800

    Getting somewhere

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-def sayHello():
-    print("Hello, World!")
+def main():
+    students = ["Mowgli", "Bagheera", "Sher Khan", "Baalu"]
+
+    for student in students:
+        print(student.upper())
+
+if __name__ == "__main__":
+    main()
