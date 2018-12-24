066990284b58faeb807700865946aecc6c7b638f
vmkottoo <venu.m.kottooru@intel.com>
2018-12-24 13:41:08 -0800

    Tic Tac Toe

--- test.py
+++ test.py
@@ -1,7 +1,39 @@
-def main(**kwargs):
-        print("Number of arguments passed : {0}".format(len(kwargs)))
-        print("Type of arguments : {0}".format(type(kwargs)))
-        print("Contents of arguments:\n{0}".format(kwargs))
+class name(object):
+        def __init__(self, first_name, middle_name, last_name):
+                self.FirstName = first_name
+                self.MiddleName = middle_name
+                self.LastName = last_name
+        
+
+class student(name):
+        def __init__(self, first_name, middle_name, last_name, student_id):
+                name.__init__(self, first_name, middle_name, last_name)
+                self.StudentId = student_id
+
+def readStudents():
+        with open("student.txt", "r") as f:
+                for s in f.readlines():
+                        yield s
+
+
+def writeStudent():
+        first_name = input("Input student first name: ")
+        middle_name = input("Input student middle name: ")
+        last_name = input("Input student last name: ")
+        student_id = input("Input student id: ")
+
+        st = student(first_name, middle_name, last_name, student_id)
+
+        with open("student.txt", "a") as f:
+                f.write(str(st.__dict__) + "\n")
+
+
+def main():
+        writeStudent()
+
+        for student in readStudents():
+                print(student)
+
 
 if __name__ == "__main__":
-    main(first_name = "Venu", last_name = "Kottooru", employee_id = "106224098")
+    main()
